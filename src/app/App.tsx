c59f0899a2a8a666891c3766081c12b1d25d4dfb
Bogdan-67 <tugay.bogdan03@mail.ru>
2024-01-16 22:15:33 +0300

    добавил интернационализацию

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React, { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import './styles/index.scss';
@@ -12,11 +11,13 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className={'content-page'}>
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback={''}>
+                <Navbar />
+                <div className={'content-page'}>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
---
+++ src/widgets/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,27 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './LangSwitcher.module.scss';
+import { useTranslation } from 'react-i18next';
+import React from 'react';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+
+interface LangSwitcherProps {
+    className?: string;
+}
+
+export const LangSwitcher = ({ className }: LangSwitcherProps) => {
+    const { t, i18n } = useTranslation();
+    // TODO: Сделать сохранение выбранного языка в локальное хранилище
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+    };
+
+    return (
+        <Button
+            className={classNames(cls.langSwitcher, {}, [className])}
+            theme={ThemeButton.CLEAR}
+            onClick={toggle}
+        >
+            {t('Язык')}
+        </Button>
+    );
+};
