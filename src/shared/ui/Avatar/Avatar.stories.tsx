47a5b29993aa9bd3b8e9f35cdb176eff8ee43a3c
Bogdan-67 <tugaybogdan03@gmail.com>
2025-01-10 17:30:43 +0300

    avatar component

---
+++ src/shared/ui/Avatar/Avatar.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import AvatarImg from './storybook.jpg';
+import { CenterDecorator } from 'shared/config/storybook/CenterDecorator';
+
+const meta: Meta<typeof Avatar> = {
+    title: 'shared/Avatar',
+    component: Avatar,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    args: {
+        src: AvatarImg,
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof Avatar>;
+
+export const Size300: Story = {
+    args: {
+        size: 300,
+    },
+    decorators: [CenterDecorator],
+};
+
+export const Size150: Story = {
+    args: {
+        size: 150,
+    },
+    decorators: [CenterDecorator],
+};
+
+export const Size50: Story = {
+    args: {
+        size: 50,
+    },
+    decorators: [CenterDecorator],
+};
